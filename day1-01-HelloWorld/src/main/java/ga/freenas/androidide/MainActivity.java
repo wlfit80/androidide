240c361143690585257acd34229ad29876054e22
wlfit80 <wlfit80@gmail.com>
2019-11-18 11:45:31 +0800

    day1

---
+++ day1-01-HelloWorld/src/main/java/ga/freenas/androidide/MainActivity.java
@@ -0,0 +1,13 @@
+package ga.freenas.androidide;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
